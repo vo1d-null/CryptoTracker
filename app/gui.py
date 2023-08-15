ef70b54b59fcb7e816a3796d1a6ab510f3559a99
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2023-08-15 03:07:38 +0300

    -Fixed update button.

--- app/gui.py
+++ app/gui.py
@@ -1,6 +1,6 @@
 # app/gui.py
 import wx
-from app.api import get_crypto_prices  # Import the get_crypto_prices function
+from app.api import get_crypto_prices
 
 
 class CryptoPriceFrame(wx.Frame):
@@ -8,14 +8,24 @@ class CryptoPriceFrame(wx.Frame):
         super(CryptoPriceFrame, self).__init__(parent, title=title, size=(400, 300))
 
         panel = wx.Panel(self)
+        sizer = wx.GridBagSizer(5, 5)
 
-        self.label = wx.StaticText(panel, label="Cryptocurrency Prices", pos=(10, 10))
-        self.update_button = wx.Button(panel, label="Update Prices", pos=(10, 40))
-        self.result_text = wx.TextCtrl(panel, style=wx.TE_MULTILINE | wx.TE_READONLY, pos=(10, 70), size=(380, 180))
+        title_label = wx.StaticText(panel, label="Cryptocurrency Prices")
+        sizer.Add(title_label, pos=(0, 0), span=(1, 2), flag=wx.ALIGN_CENTER_VERTICAL)
+
+        update_button = wx.Button(panel, label="Update Prices")
+        sizer.Add(update_button, pos=(1, 0), flag=wx.ALIGN_LEFT)
+
+        self.result_text = wx.TextCtrl(panel, style=wx.TE_MULTILINE | wx.TE_READONLY)  # Store as an instance attribute
+        sizer.Add(self.result_text, pos=(2, 0), span=(1, 2), flag=wx.EXPAND)
 
         # Bind the button click event to the update_prices method
-        self.update_button.Bind(wx.EVT_BUTTON, self.update_prices)
+        update_button.Bind(wx.EVT_BUTTON, self.update_prices)
 
+        sizer.AddGrowableCol(1)
+        sizer.AddGrowableRow(2)
+
+        panel.SetSizerAndFit(sizer)
         self.update_prices()
 
     def update_prices(self, event=None):
@@ -23,7 +33,7 @@ class CryptoPriceFrame(wx.Frame):
         self.result_text.Clear()
 
         for crypto, data in prices.items():
-            price = data.get("usd", "N/A")
+            price = data.get("usd","N/A")
             self.result_text.AppendText(f"{crypto}: ${price}\n")
 
         if event is None:
